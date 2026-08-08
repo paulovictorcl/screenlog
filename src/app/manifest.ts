cb2b36aee927f76c14b8228a91afffe0b16c9aa5
Desenvolvedor <dev@screenlog.com>
2026-08-07 23:13:05 -0300

    Fix: ajusta regras de tamanho do icone no manifest para forçar instalação no Android

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,14 +12,22 @@ export default function manifest(): MetadataRoute.Manifest {
     icons: [
       {
         src: '/app-icon.png',
+        sizes: 'any',
+        type: 'image/png',
+        purpose: 'any maskable'
+      },
+      {
+        src: '/app-icon.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'any maskable'
       },
       {
         src: '/app-icon.png',
         sizes: '512x512',
         type: 'image/png',
-      },
+        purpose: 'any maskable'
+      }
     ],
   };
 }
