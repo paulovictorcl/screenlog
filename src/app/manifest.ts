a0c180d5481ea38eb897051fb9049f449d9b4b59
Desenvolvedor <dev@screenlog.com>
2026-08-07 23:25:22 -0300

    Fix: usa script nativo para gerar icones no tamanho absoluto (192 e 512) para o PWA

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,22 +11,22 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#de3940',
     icons: [
       {
-        src: '/app-icon.png',
-        sizes: 'any',
-        type: 'image/png',
-        purpose: 'maskable'
-      },
-      {
-        src: '/app-icon.png',
+        src: '/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable'
       },
       {
-        src: '/app-icon.png',
+        src: '/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable'
+      },
+      {
+        src: '/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any'
       }
     ],
   };
